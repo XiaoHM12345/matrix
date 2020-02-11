061a35a168deaef5a360dfe0151316361d2b13b6
XiaoHM12345 <60917454+XiaoHM12345@users.noreply.github.com>
2020-02-11 18:30:28 +0800

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,25 +1,34 @@
 #pragma once
 #include<initializer_list>
-#include<string>
 #include<vector>
+#include<exception>
+
+class dimension_mismatch : public std::exception
+{
+public:
+	const char* what() const throw()
+	{
+		return "the dimension of two matrix do not match!";
+	}
+};
 
 using namespace std;
 
 class matrix
 {
 public:
-	//构造函数
+	//构造与析构
 	matrix(int i, int j);
 	matrix(int i, int j, initializer_list<double> input);
 	matrix(const matrix& A);
 	~matrix();
 	matrix& operator =(const matrix& A);
-	//运算符重载
+	//重载运算符
 	matrix operator +(const matrix& A);
 	matrix operator -(const matrix& A);
 	matrix operator *(const matrix& A);
 	double& operator ()(int i, int j) const;
-
+	//获取矩阵维度
 	int get_row() const;
 	int get_column() const;
 
